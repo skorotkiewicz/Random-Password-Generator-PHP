f384a69fa6658c2ca60cd8127074bb99f6fd2922
Sebastian <admin@itunix.eu>
2011-11-13 08:33:49 +0100

    Readme

--- index.php
+++ index.php
@@ -1,352 +1,172 @@
-<?php
-/*
---- Random Password Generator PHP ---
---- Written by modInfo ---
-*/
-?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" >
-<head runat="server">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>Random Password Generator</title>
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<meta id="viewport" name="viewport" content="width=320; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
-<link rel="stylesheet" href="iphone.css" />
-<link rel="apple-touch-icon" href="images/apple-touch-icon.png" />
+<link type="text/css" href="css/style.css" rel="stylesheet" />
+<link rel="icon" type="image/png" href=""/>
+<meta name="description" content="" />
+<meta name="keywords" content="" />
 </head>
 <body>
 
-    <form id="form1" runat="server">
-        <div id="intro">
-<?php
+<div class="wrapper">
+
+	<div class="logo">
+		<h2>Random Password Generator</h2>
+	</div>
 
 
-function abc3($howlong)
-{
-		$howlong = $_GET['l'];
-		if($howlong >= 1 and $howlong <= 20) {
-		$howlong = $_GET['l'];
-		}
-		else {
-		$howlong = 3;
-		}
+	<div class="counter">
+	
 
-// $howlong = $_GET['l'];
-$chars .= "ABCDEFGHIJKLMNOPRSTUWZYXQ";
-$pass = "";
-$len = strlen($chars) - 1;
-for($i =0; $i < $howlong; $i++)
-  {
-   $random = rand(0, $len);
-
-       $output .=  $chars[$random];
-   }
-return $output;
-};
+	<div class="title">
+		<?php if ($_GET['s'] and $_GET['l']) { ?>
+		Your generated password here:
+		<?php } else { ?>
+		Select Password length and then Symbol
+		<?php } ?>
+	</div>
 
 
 
-function abc2($howlong)
+	<div class="password">
+<?php include('function.php'); 
+
+
+$types = array(0 => 'ABC', 'aBc', 'abc', 'a3', 'a3c', '123', 'abc');
+
+
+switch ($_GET['s'])
 {
 
-		$howlong = $_GET['l'];
-		if($howlong >= 1 and $howlong <= 20) {
-		$howlong = $_GET['l'];
-		}
-		else {
-		$howlong = 3;
-		}
+	case 'ABC':
+		echo abc3($password);
+		break;
 
-// $howlong = $_GET['l'];
-$chars = "abcdefghijklmnoprstuwxyzq"; 
-$chars .= "ABCDEFGHIJKLMNOPRSTUWZYXQ"; 
-$pass = ""; 
-$len = strlen($chars) - 1; 
-for($i =0; $i < $howlong; $i++) 
-  { 
-   $random = rand(0, $len); 
+	case 'aBc':
+		echo abc2($password);
+		break;
 
-       $output .=  $chars[$random]; 
-   } 
-return $output; 
-}; 
+	case 'abc':
+		echo abc1($password);
+		break;
 
+	case 'a3':
+		echo abcmix($password);
+		break;
 
-function abc1($howlong) 
-{ 
-		$howlong = $_GET['l'];
-		if($howlong >= 1 and $howlong <= 20) {
-		$howlong = $_GET['l'];
-		}
-		else {
-		$howlong = 3;
-		}
+	case 'a3c':
+		echo a3c($password);
+		break;
 
-// $howlong = $_GET['l'];
-$chars = "abcdefghijklmnoprstuwxyzq"; 
-$pass = ""; 
-$len = strlen($chars) - 1; 
-for($i =0; $i < $howlong; $i++) 
-  { 
-   $random = rand(0, $len); 
+	case '123':
+		echo a123($password);
+		break;
 
-       $output .=  $chars[$random]; 
-   } 
-return $output; 
-}; 
+	case 'abc':
+		echo abc($password);
+		break;
 
-
-function abcmix($howlong) 
-{ 
-		$howlong = $_GET['l'];
-		if($howlong >= 1 and $howlong <= 20) {
-		$howlong = $_GET['l'];
-		}
-		else {
-		$howlong = 3;
-		}
-
-// $howlong = $_GET['l'];
-$chars = "abcdefghijklmnoprstuwxyzq"; 
-$chars .= "ABCDEFGHIJKLMNOPRSTUWZYXQ"; 
-$chars .= "1234567890"; 
-$chars .= "~`!@#$%^&*()_+-={}[]:\";'<>?,./\|";
-$pass = ""; 
-$len = strlen($chars) - 1; 
-for($i =0; $i < $howlong; $i++) 
-  { 
-   $random = rand(0, $len); 
-
-       $output .=  $chars[$random]; 
-   } 
-return $output; 
-};
-
-
-function a3c($howlong) 
-{ 
-		$howlong = $_GET['l'];
-		if($howlong >= 1 and $howlong <= 20) {
-		$howlong = $_GET['l'];
-		}
-		else {
-		$howlong = 3;
-		}
-
-// $howlong = $_GET['l'];
-$chars = "abcdefghijklmnoprstuwxyzq"; 
-$chars .= "ABCDEFGHIJKLMNOPRSTUWZYXQ"; 
-$chars .= "1234567890"; 
-$pass = ""; 
-$len = strlen($chars) - 1; 
-for($i =0; $i < $howlong; $i++) 
-  { 
-   $random = rand(0, $len); 
-
-       $output .=  $chars[$random]; 
-   } 
-return $output; 
-};
-
-
-function a123($howlong) 
-{ 
-		$howlong = $_GET['l'];
-		if($howlong >= 1 and $howlong <= 20) {
-		$howlong = $_GET['l'];
-		}
-		else {
-		$howlong = 3;
-		}
-
-// $howlong = $_GET['l'];
-$chars .= "1234567890"; 
-$pass = ""; 
-$len = strlen($chars) - 1; 
-for($i =0; $i < $howlong; $i++) 
-  { 
-   $random = rand(0, $len); 
-
-       $output .=  $chars[$random]; 
-   } 
-return $output; 
-};
-
-
-function abc($howlong) 
-{ 
-		$howlong = $_GET['l'];
-		if($howlong >= 1 and $howlong <= 20) {
-		$howlong = $_GET['l'];
-		}
-		else {
-		$howlong = 3;
-		}
-
-// $howlong = $_GET['l'];
-$chars = "abcdefghijklmnoprstuwxyzq"; 
-$pass = ""; 
-$len = strlen($chars) - 1; 
-for($i =0; $i < $howlong; $i++) 
-  { 
-   $random = rand(0, $len); 
-
-       $output .=  $chars[$random]; 
-   } 
-return $output; 
-};
-
-
-
-
-echo '';
-
-if ($_GET['s'] == 'ABC'){
-echo 'Your generated password here: <h2>'.abc3($password).'</h2>';
+	default:
+		echo '';
 }
+?>
 
-elseif ($_GET['s'] == 'aBc'){
-echo 'Your generated password here: <h2>'.abc2($password).'</h2>';
-}
+	</div>
 
-elseif ($_GET['s'] == 'abc'){
-echo ' Your generated password here: <h2>'.abc1($password).'</h2>';
-}
+<?php	if (isset($_GET['l'])) 
+	{ 
+?>
 
-elseif ($_GET['s'] == 'a3'){
-echo 'Your generated password here: <h2>'.abcmix($password).'</h2>';
-}
+	<div class="types">
+		Symbols used:
+	<p>
 
-elseif ($_GET['s'] == 'a3c'){
-echo 'Your generated password here: <h2>'.a3c($password).'</h2>';
-}
+	<?php
+		for ($i = 0; $i < 6; $i++)
+		{
 
-elseif ($_GET['s'] == '123'){
-echo 'Your generated password here: <h2>'.a123($password).'</h2>';
-}
+			if ($_GET['s'] == $types[$i])
+			{ 
+				echo '<a class="selected button" href="?s='.$types[$i].'&amp;l='.$_GET['l'].'">'.reMe($types[$i]).'</a>';
+			}
+			else
+			{
+				echo '<a class="button" href="?s='.$types[$i].'&amp;l='.$_GET['l'].'">'.reMe($types[$i]).'</a>';
+			}
+	
 
-elseif ($_GET['s'] == 'abc'){
-echo 'Your generated password here: <h2>'.abc($password).'</h2>';
-}
+		}
 
-else{
-echo '<br />Select Password length,<br /> then Symbol<br /><br />';
-}
+	?>
 
-if (isset($_GET['l'])){
-echo 'Symbols used: <h3>';
+	</p>
+	</div>
 
-if ($_GET['s'] == 'abc') 
-{ echo '<a class="selected" href="?s=abc&amp;l='.$_GET['l'].'">abc</a>'; }
-else { echo '<a class="la" href="?s=abc&amp;l='.$_GET['l'].'">abc</a>'; }
+	<?php
+	} 
 
-if ($_GET['s'] == '123')
-{ echo '<a class="selected" href="?s=123&amp;l='.$_GET['l'].'">123</a>'; }
-else { echo '<a class="la" href="?s=123&amp;l='.$_GET['l'].'">123</a>'; }
+	else 
+	{ 
 
-if ($_GET['s'] == 'a3c')
-{ echo '<a class="selected" href="?s=a3c&amp;l='.$_GET['l'].'">a3c</a>'; }
-else { echo '<a class="la" href="?s=a3c&amp;l='.$_GET['l'].'">a3c</a>'; }
+		echo 'Symbols used: <h3>';
 
 
-if ($_GET['s'] == 'a3')
-{ echo '<a class="selected" href="?s=a3&amp;l='.$_GET['l'].'">a3#</a>'; }
-else { echo '<a class="la" href="?s=a3&amp;l='.$_GET['l'].'">a3#</a>'; }
+		for ($i = 0; $i < 6; $i++)
+		{
+
+			echo '<a href="?s='.$types[$i].'" class="button">'.reMe($types[$i]).'</a>';
+
+		}
 
 
-if ($_GET['s'] == 'aBc')
-{ echo '<a class="selected" href="?s=aBc&amp;l='.$_GET['l'].'">aBc</a>'; }
-else { echo '<a class="la" href="?s=aBc&amp;l='.$_GET['l'].'">aBc</a>'; }
+		echo '</h3>';
 
-if ($_GET['s'] == 'ABC')
-{ echo '<a class="selected" href="?s=ABC&amp;l='.$_GET['l'].'">ABC</a>'; }
-else { echo '<a class="la" href="?s=ABC&amp;l='.$_GET['l'].'">ABC</a>'; }
-
-
-echo '</h3>';
-}
-
-else{
-
-echo '
-Symbols used: 
-<h3>
-<a class="la" href="?s=abc">abc</a> 
-<a class="la" href="?s=123">123</a> 
-<a class="la" href="?s=a3c">a3c</a> 
-<a class="la" href="?s=a3">a3#</a> 
-<a class="la" href="?s=aBc">aBc</a> 
-<a class="la" href="?s=ABC">ABC</a>
-</h3>
-';
-}
-
-
+	} 
 
 echo '<br />';
 
+if (isset($_GET['s'])) 
+{ 
+		echo 'Password length:  <h3>';
 
 
-if (isset($_GET['s'])){
+		for ($i = 6; $i < 13; $i++) 
+		{
+	
+			if ($_GET['l'] == $i) 
+			{ 
+				echo '<a href="?s='.$_GET['s'].'&amp;l='.$i.'" class="selected button">'.$i.'</a>';
+			}
+			else 
+			{
+				 echo '<a class="button" href="?s='.$_GET['s'].'&amp;l='.$i.'"> '.$i.' </a> ';
+			}
 
-echo 'Password length:  <h3>';
-
-if ($_GET['l'] == 6)
-{ echo '<a class="selected" href="?s='.$_GET['s'].'&amp;l=6"> 6 </a>'; }
-else { echo '<a class="la" href="?s='.$_GET['s'].'&amp;l=6"> 6 </a>'; }
-#####################################
-if ($_GET['l'] == 7)
-{ echo '<a class="selected" href="?s='.$_GET['s'].'&amp;l=7"> 7 </a>'; }
-else { echo '<a class="la" href="?s='.$_GET['s'].'&amp;l=7"> 7 </a>'; }
-#####################################
-if ($_GET['l'] == 8)
-{ echo '<a class="selected" href="?s='.$_GET['s'].'&amp;l=8"> 8 </a>'; }
-else { echo '<a class="la" href="?s='.$_GET['s'].'&amp;l=8"> 8 </a>'; }
-#####################################
-if ($_GET['l'] == 9)
-{ echo '<a class="selected" href="?s='.$_GET['s'].'&amp;l=9"> 9 </a>'; }
-else { echo '<a class="la" href="?s='.$_GET['s'].'&amp;l=9"> 9 </a>'; }
-#####################################
-if ($_GET['l'] == 10)
-{ echo '<a class="selected" href="?s='.$_GET['s'].'&amp;l=10"> 10 </a>'; }
-else { echo '<a class="la" href="?s='.$_GET['s'].'&amp;l=10"> 10 </a>'; }
-#####################################
-if ($_GET['l'] == 11)
-{ echo '<a class="selected" href="?s='.$_GET['s'].'&amp;l=11"> 11 </a>'; }
-else { echo '<a class="la" href="?s='.$_GET['s'].'&amp;l=11"> 11 </a>'; }
-#####################################
-if ($_GET['l'] == 12)
-{ echo '<a class="selected" href="?s='.$_GET['s'].'&amp;l=12"> 12 </a>'; }
-else { echo '<a class="la" href="?s='.$_GET['s'].'&amp;l=12"> 12 </a>'; }
+		}
 
 
-echo '</h3> ';
-}
+ } else { ?> 
 
-else{
 
-echo '
+	<div class="length">
+		Password length:
+	<p>
 
-Password length:  <h3> 
-<a class="la" href="?l=6"> 6 </a>  
-<a class="la" href="?l=7"> 7 </a>  
-<a class="la" href="?l=8"> 8 </a>  
-<a class="la" href="?l=9"> 9 </a>  
-<a class="la" href="?l=10"> 10 </a>  
-<a class="la" href="?l=6"> 11 </a>  
-<a class="la" href="?l=12"> 12 </a> 
-</h3>
-';
-}
+	<?php
+		for ($i = 6; $i < 13; $i++) 
+		{
+			echo '<a class="button" href="?l='.$i.'"> '.$i.' </a>';  
 
-?>
-        </div>
-	    <div class="footer">
-	    
-	       Code by ITUnix.eu 2011 <br /> Design iPhone Version. &copy; Peter Hinton Design, 2011
-	    
-	    </div>
-    
-    </form>
+		}
+	?>
+
+	</p>
+	</div>
+<?php } ?>
+	</div>
+</div>
+
 </body>
 </html>
-
